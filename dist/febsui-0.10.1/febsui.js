ff779437baf57533c495aa0f5ed67fd0530f9249
lipengxiang <lipengxiang@tjitech.cn>
2018-07-07 15:13:35 +0800

    [enhancment] improve css

--- dist/febsui-0.10.1/febsui.js
+++ dist/febsui-0.10.1/febsui.js
@@ -4662,6 +4662,9 @@ function swiper_init(elem) {
 
       if (dom.hasClass('febsui-swiper-vertical')) {
         pages[0].__swiper_vertical = true;
+        dom.css('touch-action', 'pan-y');
+      } else {
+        dom.css('touch-action', 'pan-x');
       }
 
       setTimeout(function () {
